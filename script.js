8a6d7027a3ca3ab25a45853800053cbc2dd15b3d
daOrkz <80348879+daOrkz@users.noreply.github.com>
2022-03-11 22:40:13 +0700

    News App with usage API
    News App, usage API

---
+++ script.js
@@ -0,0 +1,107 @@
+
+const newsContainer      = document.querySelector('.flex-news')
+
+const form               = document.forms['form-news']
+const serchNews          = form.elements['serch-news']
+const selectCategory     = form.elements['category']
+const selectCountry      = form.elements['country']
+const searcBtn           = form.elements['search-btn']
+
+
+function queryHTTP(url, method = 'GET', body = '') {
+
+  const xhr = new XMLHttpRequest()
+
+  xhr.open(method, url)
+  xhr.setRequestHeader('Content-Type', 'application/json')
+
+  xhr.onload = () => {
+    const response = JSON.parse(xhr.response)
+    renderNews(response.articles)
+  }
+
+  xhr.send(JSON.stringify(body))
+}
+
+const createURL = {
+  apiKey: 'f57c9d6d35e745bb9c571443b47954bb',
+  apiURL: 'https://news-api-v2.herokuapp.com',
+
+  everyThing(query = 'bitcoin') {
+    return `
+      ${this.apiURL}/everything?q=${query}&apiKey=${this.apiKey}
+    `
+  },
+  headlines(country = 'us', category = '') {
+    return `
+      ${this.apiURL}/top-headlines?country=${country}&category=${category}&apiKey=${this.apiKey}
+    `
+  }
+}
+
+function createURLaaaaaaaaa(country = 'us', category = '') {
+  const apiKey = 'f57c9d6d35e745bb9c571443b47954bb'
+  const apiURL = 'https://news-api-v2.herokuapp.com'
+
+  // let url = 'https://news-api-v2.herokuapp.com/everything?q=bitcoin&apiKey=' + apiUrl
+  // let url = 'https://news-api-v2.herokuapp.com/top-headlines?country=us&category=sports&apiKey=' + apiKey
+  let url = `${apiURL}/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}`
+
+  console.log(url);
+  return url
+}
+
+function renderNews(newsArr) {
+  let container = ''
+  newsContainer.innerHTML = ' '
+
+  newsArr.forEach(news => {
+    container += newsHTML(news)
+  });
+
+  newsContainer.insertAdjacentHTML('afterbegin', container)
+}
+
+function getOneNews() {
+
+}
+
+function newsHTML(news) {
+  return `
+  <div class="card-news">
+    <img src="${news.urlToImage}" alt="">
+    <h2>${news.title}</h2>
+    <h3>${news.description}</h3>
+    <a href="${news.url}"><b>Read More</b></a>
+  </div>
+  `
+}
+
+form.onsubmit = (e) => {
+  e.preventDefault()
+  const formValue = getFormValue()
+
+  if(formValue.serchNewsValue) {
+    queryHTTP(createURL.everyThing(formValue.serchNewsValue), 'GET', formValue.serchNewsValue)
+  } else {
+    queryHTTP(createURL.headlines(formValue.selectCountryValue, formValue.selectCategoryValue))
+  }
+  clearInput()
+}
+
+function getFormValue() {
+  return {
+    serchNewsValue: serchNews.value,
+    selectCategoryValue: selectCategory.value,
+    selectCountryValue: selectCountry.value,
+  }
+}
+
+function clearInput() {
+  serchNews.value = ''
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  queryHTTP(createURL.headlines())
+})
